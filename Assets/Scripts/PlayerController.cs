0ca906b4819e9a0575153d1a82727531e5445cc1
Hesler203 <165882615+Hesler203@users.noreply.github.com>
2026-08-06 00:02:28 -0500

    feature: created a player controller script
    added references to the sprite renderer and rigidbody components on the player
    
    added reference to the input manager singleton for input reading.
    
    Player move input values determine the sprite direction flipX attribute and the movement direction for the rigidbody force to be applied for movement
    
    movement speed, start position, and move deadzone set in the inspector

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,60 @@
+using System;
 using UnityEngine;
-
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private InputManager inputManager;
+    private SpriteRenderer sprite;
+    private Rigidbody rb;
+
+    [Header("Settings")]
+    [SerializeField] private Vector3 startPosition;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float moveDeadZone;
+
     void Start()
     {
-        
+        inputManager = InputManager.Instance;
+        moveDeadZone = inputManager.MoveDeadzone;
+        sprite = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        FlipSpriteOnMove();
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    private void FlipSpriteOnMove()
+    {
+        if (inputManager.PlayerMoveInput.x > moveDeadZone)
+        {
+            sprite.flipX = true;
+        }
+        else if (inputManager.PlayerMoveInput.x < -moveDeadZone)
+        {
+            sprite.flipX = false;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        if (Math.Abs(inputManager.PlayerMoveInput.sqrMagnitude) > moveDeadZone)
+        {
+            //Vector3 moveDirection = new Vector3(inputManager.PlayerMoveInput.x, 0, inputManager.PlayerMoveInput.y);
+            Vector3 moveDirection = inputManager.PlayerMoveInput;
+            Vector3 velocity = moveDirection.normalized * moveSpeed;
+
+            rb.AddForce(velocity, ForceMode.Force);
+        }
     }
 }
