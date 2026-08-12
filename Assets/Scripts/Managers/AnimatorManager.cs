8d350b061023f859c27c36e26dea7c9d7015132d
Hesler203 <165882615+Hesler203@users.noreply.github.com>
2026-08-11 22:58:35 -0500

    feature: added a Seagull enemy with animations
    seagull enemy added with animations, collider, and rigidbody

--- Assets/Scripts/Managers/AnimatorManager.cs
+++ Assets/Scripts/Managers/AnimatorManager.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 public class AnimatorManager : MonoBehaviour
 {
     [field: SerializeField] public Animator PlayerAnimator { get; private set; }
-
+    [field: SerializeField] public Animator SeagullAnimator { get; private set; }
 }
