cdc032288c77607b8d5824224509e3b5036fe9a3
Hesler203 <165882615+Hesler203@users.noreply.github.com>
2026-08-14 00:06:27 -0500

    feature: added a video player to the camera & imported a video clip to play on game round end
    added a coroutine to the camera controller script which plays a video clip when a play flag is flipped

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,31 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform playerTransform;
+    public VideoPlayer VideoPlayer { get; private set; }
+    public bool play = false;
+    void Start()
+    {
+        VideoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    void Update()
+    {
+        if (play)
+        {
+            StartCoroutine(nameof(PlayWaveClip));
+        }
+    }
+
+    IEnumerator PlayWaveClip()
+    {
+        VideoPlayer.Play();
+        yield return new WaitForSeconds((float)VideoPlayer.length);
+
+        VideoPlayer.Stop();
+        play = false;
+        StopCoroutine(nameof(PlayWaveClip));
+    }
 }
